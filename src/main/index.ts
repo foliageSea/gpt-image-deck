e50938f39c7c5a4f49fb4fc67d0b063e08e6c8da
foliageSea <1742262457@qq.com>
2026-08-29 00:10:06 +0800

    chore: set application id

--- src/main/index.ts
+++ src/main/index.ts
@@ -54,7 +54,7 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron.app')
+  electronApp.setAppUserModelId('com.foliage')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
